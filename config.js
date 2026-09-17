b615f412bd18c3df1768828b2e6c836be57c108c
AquaBill JO <aquabilljo@gmail.com>
2026-09-17 23:26:24 +0300

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ const APP_CONFIG = {
   /* ---------- معلومات التطبيق العامة ---------- */
   appName: 'AquaBill JO',
   version: '1.0.0',
-  cacheVersion: 'aquabill-jo-v12.5',
+  cacheVersion: 'aquabill-jo-v12.6',
   currencyLabelAr: 'دينار',
   complaintsPhone: '117116',
 
